894945b2967f108140954be481c9e8c036c6ce57
Łukasz Jagliński <ljaglinski@o2.pl>
2019-04-27 16:29:33 +0200

    Create Car class

---
+++ app.js
@@ -0,0 +1,13 @@
+class Car {
+    constructor(vin, brand, model, price, year, mileage, engineCapacity, hp, image) {
+        this.vin = vin;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.year = year;
+        this.mileage = mileage;
+        this. engineCapacity = engineCapacity;
+        this.hp = hp;
+        this.image = image;
+    }
+}
