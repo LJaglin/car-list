814a9a7dedf64ade7f3299ed3dffca45b01045f7
Łukasz Jagliński <ljaglinski@o2.pl>
2019-04-27 17:26:27 +0200

    Create event listener for add car and instantiete a new car object

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ class Car {
 }
 
 class UI {}
+
+//Event Listeners for add car
+document.getElementById('car-form').addEventListener('submit', (e) => {
+    //Get values from the form
+    const vin = document.getElementById('vin-number').value;
+    const brand = document.getElementById('brand').value;
+    const model = document.getElementById('model').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+    const mileage = document.getElementById('mileage').value;
+    const engineCapacity = document.getElementById('engine-capacity').value;
+    const hp = document.getElementById('hp').value;
+    const img = URL.createObjectURL(e.target.image.files[0]);
+    
+    //Instantiete new car object
+    const car = new Car(vin, brand, model, price, year, mileage, engineCapacity, hp, img);
+    console.log(car);
+
+    e.preventDefault();
+});
